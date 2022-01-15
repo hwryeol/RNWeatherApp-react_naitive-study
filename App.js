bd4a4ecf8c1e04139ba0a509f4db250a69d279c5
hwryeol <hwryeol@gmail.com>
2022-01-15 14:35:48 +0900

    add styles

--- App.js
+++ App.js
@@ -4,11 +4,47 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex:1}}>
-      <View style={{flex:1,backgroundColor:"teal"}}></View>
-      <View style={{flex:3,backgroundColor:"tomato"}}></View>
-      <View style={{flex:0.5,backgroundColor:"orange"}}></View>
+    <View style={styles.constainer}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
       
   );
 }
+
+const styles = StyleSheet.create({
+  constainer:{
+    flex:1,
+    backgroundColor: "orange"
+  },
+  city:{
+    flex:0.5,
+    justifyContent: "center",
+    alignItems:"center"
+  },
+  cityName:{
+    fontSize:48,
+    fontWeight:"500",
+  },
+  weather:{
+    flex:1,
+  },
+  day:{
+    flex: 1,
+    alignItems:"center",
+  },
+  temp:{
+    fontSize:178,
+  },
+  description:{
+    marginTop:-40,
+    fontSize: 60,
+  }
+})
